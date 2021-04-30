5e7f6239790b96775c320ffaa8143c56a45bad37
Paulo Simões <paulohbsimoes@gmail.com>
2021-04-30 17:54:00 -0300

    Desafio 28 concluído

---
+++ challenges/desafio28.js
@@ -0,0 +1,6 @@
+let empresa = "LATAM AIRLINES BRASIL";
+let natureza = "Doméstica";
+let totalVoosDomesticos = db.voos.count({ "empresa.nome": empresa, natureza });
+
+db.resumoVoos.insertOne({ empresa, totalVoosDomesticos });
+db.resumoVoos.find({ empresa }, { empresa: 1, totalVoosDomesticos: 1, _id: 0}).limit(1);
