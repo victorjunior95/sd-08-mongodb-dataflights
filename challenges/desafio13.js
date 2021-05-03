2a78217599f2aa29c8959df8c59ffb3ad2a7daca
Rita Jeveaux <ritajeveaux@gmail.com>
2021-05-03 17:37:26 -0300

    Requisitos 13-26

---
+++ challenges/desafio13.js
@@ -0,0 +1,7 @@
+db.voos.count({
+  $and: [
+    { "aeroportoDestino.continente": { $ne: "EUROPA" } },
+    { "aeroportoDestino.continente": { $ne: "ÁSIA" } },
+    { "aeroportoDestino.continente": { $ne: "OCEANIA" } },
+  ],
+});
---
+++ challenges/desafio26.js
@@ -0,0 +1,9 @@
+db.voos.deleteMany({ 
+  $and: [
+    { "empresa.nome": "GOL" }, 
+    { $and: 
+    [
+      { "passageiros.pagos": { $gte: 5, $lte: 10 } },
+    ], 
+  }], 
+});
