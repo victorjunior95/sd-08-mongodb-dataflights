23811cf2fba035c6f74b3d19eb04bf0eda22bffd
JRGCast <joao.ralph1@gmail.com>
2021-05-03 17:52:25 -0300

    resolvido desafio 26 e metade do 27

--- challenges/desafio27.js
+++ challenges/desafio27.js
@@ -3,3 +3,8 @@
 // Para isso, escreva no arquivo desafio27.js duas queries, nesta ordem:
 // Conte quantos voos da empresa PASSAREDO cujo campo natureza possua valor igual a Doméstica e crie uma query que insira na coleção resumoVoos um documento com os campos: empresa (nome da empresa) e totalVoosDomesticos (o total retornado anteriormente).
 // Em uma segunda query, retorne a empresa e o totalVoosDomesticos do primeiro documento presente na coleção resumoVoos em que a empresa seja PASSAREDO.
+
+db.voos.find(
+  { 'empresa.nome': 'PASSAREDO',
+natureza: 'Doméstica' },
+);
