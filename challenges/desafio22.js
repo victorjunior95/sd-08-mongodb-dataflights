1d74e3fe2df8e307df9e8de73a1f1ec56708db8b
Fernanda PC <nandateixmarques@gmail.com>
2021-05-03 19:45:10 -0300

    adicionando desafio 23

--- challenges/desafio22.js
+++ challenges/desafio22.js
@@ -1,8 +1,9 @@
 db.voos.find({
   $and: [
-    { "empresa.nome": { $in: [ "DELTA AIRLINES", "AMERICAN AIRLINES" ] } },
+    { "empresa.nome": { $in: ["DELTA AIRLINES", "AMERICAN AIRLINES"] } },
     { "aeroportoOrigem.sigla": "SBGR" },
     { "aeroportoDestino.sigla": "KJFK" },
   ] }, 
   { vooId: 1, _id: 0 },
-).limit(1);
+)
+.limit(1);
