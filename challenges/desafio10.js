2f7a16aab20c49a55cbef5e8a193354f901ff085
Renan Oliveira <oliveira.renan.rg@gmail.com>
2021-05-03 19:20:39 -0300

    start: 1 a 10

---
+++ challenges/desafio10.js
@@ -0,0 +1,15 @@
+db.voos.find(
+  { $and: [
+      { "nome.empresa": "GOL" },
+      { ano: 2017 },
+    ],
+  },
+  {
+    vooId: 1,
+    "empresa.nome": 1,
+    "aeroportoOrigem.nome": 1,
+    "aeroportoDestino.nome": 1,
+    mes: 1,
+    ano: 1,
+  },
+).limit(10);
