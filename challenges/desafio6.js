15673a7a0cc6095a4ce9f7b6906a4bbbe9ff4b18
emassaki <massakihirayama@gmail.com>
2021-05-03 14:51:56 -0300

    reqs 8 to 14

--- challenges/desafio6.js
+++ challenges/desafio6.js
@@ -1,7 +1,4 @@
-db.voos
-  .find(
-    { vooId: 756807 },
-    { _id: 0, vooId: 1, 'empresa.sigla': 1, 'empresa.nome': 1, passageiros: 1 },
-  )
-  .limit(3)
-  .skip(9);
+db.voos.find(
+  { vooId: 756807 },
+  { _id: 0, 'empresa.sigla': 1, 'empresa.nome': 1, passageiros: 1 },
+);
