ccd1200f6cd8313053689c0eb6e105b0280bbe60
JRGCast <joao.ralph1@gmail.com>
2021-05-03 16:39:56 -0300

    retirado .pretty() do desafio 13 de fato

--- desafio13.js
+++ desafio13.js
@@ -1,4 +1,4 @@
 // 13 - Retorne a quantidade de documentos em que o campo aeroportoDestino.continente não seja igual a EUROPA, ÁSIA e OCEANIA.
 db.voos.find(
   { 'aeroportoDestino.pais': { $ne: { $in: ['BRASIL', 'ARGENTINA', 'CHILE'] } } },
-).pretty();
+);
