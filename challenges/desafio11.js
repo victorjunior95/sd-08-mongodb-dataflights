afee9f6bcfd2e17fff1bf96e35033fe45e787070
Rafael Maltez <rafaelmaltez89@gmail.com>
2021-05-03 15:36:17 -0300

    desafio 11

---
+++ challenges/desafio11.js
@@ -0,0 +1,2 @@
+// Retorne a quantidade de documentos em que o campo aeroportoDestino.pais não seja igual a ESTADOS UNIDOS.
+db.voos.countDocuments({ 'aeroportoDestino.pais': { $ne: 'ESTADOS UNIDOS' } });
