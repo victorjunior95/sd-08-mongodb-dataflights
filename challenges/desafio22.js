68c1f0bbdabc35e932a55d8725e072ac24855fc5
victor718 <victorfelipeoliveira1@gmail.com>
2021-05-03 18:44:41 -0300

    req 17-24

---
+++ challenges/desafio22.js
@@ -0,0 +1,15 @@
+db.voos.find(
+  {
+    $and: [
+      { "empresa.nome": {
+        $in: ["DELTA AIRLINES", "AMERICAN AIRLINES"], 
+      } },
+      { "aeroportoOrigem.sigla": "SBGR" },
+      { "aeroportoDestino.sigla": "KJFK" },
+    ],
+  },
+  {
+    _id: 0,
+    vooId: 1,
+  },
+).limit(1);
---
+++ challenges/desafio23.js
@@ -0,0 +1,13 @@
+db.voos.find(
+  {
+    $and: [
+      { litrosCombustivel: { $exists: true } },
+      { litrosCombustivel: { $lte: 1000 } },
+    ],
+  },
+  {
+    _id: 0,
+    vooId: 1,
+    litrosCombustivel: 1,
+  },
+).limit(1);
