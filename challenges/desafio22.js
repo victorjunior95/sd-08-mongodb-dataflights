d6df12f62eca6db727ca7cd52253ec49cba7d2f9
liisaambrosen <liisa.ambrosen@gmail.com>
2021-05-03 17:08:36 -0300

    req 22 e 23

---
+++ challenges/desafio22.js
@@ -0,0 +1,10 @@
+db.voos.find({
+  $and: [
+    { "empresa.nome": { $in: ["DELTA AIRLINES", "AMERICAN AIRLINES"] } },
+    { "aeroportoOrigem.sigla": { $eq: "SBGR" } },
+    { "aeroportoDestino.sigla": { $eq: "KJFK" } },
+  ],
+}, {
+   vooId: 1,
+   _id: 0,
+}).limit(1);
