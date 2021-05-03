9d7a0cd08cf591474e52a1d9e4c88a7737ff1646
Fernanda PC <nandateixmarques@gmail.com>
2021-05-03 18:02:38 -0300

    corrigindo o desafio 6 - agora vai mesmo

--- challenges/desafio6.js
+++ challenges/desafio6.js
@@ -1,5 +1,8 @@
 db.voos
-.find(
-  { vooId: 756807 },
-  { "empresa.sigla": 1, "empresa.nome": 1, passageiros: 1, _id: 0 },
-  ).limit(3).skip(9);
+  .find(
+    { vooId: 756807 },
+    { _id: 0, vooId: 1, "empresa.sigla": 1, "empresa.nome": 1, passageiros: 1 },
+  )
+  .limit(3)
+  .skip(9);
+  
