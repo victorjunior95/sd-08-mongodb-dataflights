ce83c9996cc97f88bf27407b3084987edfae0e22
emassaki <massakihirayama@gmail.com>
2021-05-03 15:11:09 -0300

    reqs 22 to 28

---
+++ challenges/desafio27.js
@@ -0,0 +1,10 @@
+db.resumoVoos.insertOne({
+  empresa: 'PASSAREDO',
+  totalVoosDomesticos: db.voos
+    .find({ 'empresa.nome': 'PASSAREDO', natureza: 'Doméstica' })
+    .count(),
+});
+db.resumoVoos.find(
+  { empresa: 'PASSAREDO' },
+  { empresa: 1, totalVoosDomesticos: 1, _id: 0 },
+);
