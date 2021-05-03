ef76a8f8db75801ffff12f2df70ead58893f96fe
Gabriel-Goncalves <gabriel_98gm@hotmail.com>
2021-05-03 15:31:19 -0300

    adicionando desafio19 ao 24

---
+++ challenges/desafio24.js
@@ -0,0 +1,12 @@
+db.voos
+  .find(
+    {
+      $and: [
+        { litrosCombustivel: { $not: { $gt: 600 } } },
+        { 'empresa.nome': { $nin: ['GOL', 'AZUL'] } },
+        { litrosCombustivel: { $exists: true } },
+      ],
+    },
+    { _id: 0, vooId: 1, litrosCombustivel: 1, 'empresa.nome': 1 },
+  )
+  .limit(1);
