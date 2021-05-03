a19855ab14e68972ed79c8963d8ae32fe4326d42
JRGCast <joao.ralph1@gmail.com>
2021-05-03 18:50:50 -0300

    adicionado litrosCombustivel para mostrar no desafio 23

--- challenges/desafio23.js
+++ challenges/desafio23.js
@@ -1,4 +1,4 @@
 // 23 - Retorne o vooId e litrosCombustivel do primeiro voo em que o campo litrosCombustivel não seja maior do que 1000 e o campo litrosCombustivel exista.
 db.voos.findOne({
   litrosCombustivel: { $exists: 1, $not: { $gt: 1000 } } },
-{ vooId: 1, _id: 0 });
+{ vooId: 1, litrosCombustivel: 1, _id: 0 });
