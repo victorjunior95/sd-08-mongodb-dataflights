cb14246a6200d9911512a6de67cf97ec68fa187c
Douglas Cajueiro <dev.douglas.cajueiro@gmail.com>
2021-05-03 17:16:11 -0300

    Challenge-23-done

---
+++ challenges/desafio23.js
@@ -0,0 +1,8 @@
+db.voos.find(
+  { $and:
+    [{ litrosCombustivel: { $exists: true } },
+    { litrosCombustivel: { $lte: 1000 } }, 
+    ],
+  },
+  { _id: 0, vooId: 1, litrosCombustivel: 1 },
+).limit(1);
