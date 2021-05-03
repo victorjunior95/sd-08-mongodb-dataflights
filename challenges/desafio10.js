347c01c4018257add5f192f24593dd582c04aa79
MEianelli <filipelouzano@gmail.com>
2021-05-03 18:58:55 -0300

    Requisitos 10 ao 18

---
+++ challenges/desafio10.js
@@ -0,0 +1,1 @@
+db.voos.find({ $and: [{ "empresa.nome": "GOL" }, { ano: 2017 }] }, { vooId: 1, "empresa.nome": 1, _id: 0, "aeroportoOrigem.nome": 1, "aeroportoDestino.nome": 1, mes: 1, ano: 1 }).limit(10);
---
+++ challenges/desafio12.js
@@ -0,0 +1,1 @@
+db.voos.find({ $or: [{ "aeroportoDestino.pais": "BRASIL" }, { "aeroportoDestino.pais": "ARGENTINA" }, { "aeroportoDestino.pais": "CHILE" }] }).count();
