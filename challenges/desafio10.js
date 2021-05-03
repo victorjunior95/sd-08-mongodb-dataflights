d45cf21869011bf004cca77eae0f45df66956e23
Daniel <danieldsrd@gmail.com>
2021-05-03 15:47:32 -0300

    Ate desafio 13

--- challenges/desafio10.js
+++ challenges/desafio10.js
@@ -7,7 +7,7 @@ db.voos.find(
     vooId: 1,
     'empresa.nome': 1, 
     'aeroportoOrigem.nome': 1, 
-    'aeroportoDestino.pais': 1, 
+    'aeroportoDestino.nome': 1, 
     mes: 1, 
     ano: 1,
     _id: 0,
