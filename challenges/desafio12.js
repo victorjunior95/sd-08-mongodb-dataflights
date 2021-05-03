04e8bf59811c04405a6c5ffeec9293b53f06cfe5
MEianelli <filipelouzano@gmail.com>
2021-05-03 19:06:09 -0300

    Requisitos 10 ao 21

--- challenges/desafio12.js
+++ challenges/desafio12.js
@@ -1,1 +1,3 @@
-db.voos.find({ $or: [{ "aeroportoDestino.pais": "BRASIL" }, { "aeroportoDestino.pais": "ARGENTINA" }, { "aeroportoDestino.pais": "CHILE" }] }).count();
+db.voos.find({ $or: [{ "aeroportoDestino.pais": "BRASIL" }, 
+{ "aeroportoDestino.pais": "ARGENTINA" }, 
+{ "aeroportoDestino.pais": "CHILE" }] }).count();
--- challenges/desafio13.js
+++ challenges/desafio13.js
@@ -1,1 +1,3 @@
-db.voos.find({ $nor: [{ "aeroportoDestino.continente": "EUROPA" }, { "aeroportoDestino.continente": "ÁSIA" }, { "aeroportoDestino.continente": "OCEANIA" }] }).count();
+db.voos.find({ $nor: [{ "aeroportoDestino.continente": "EUROPA" }, 
+{ "aeroportoDestino.continente": "ÁSIA" }, 
+{ "aeroportoDestino.continente": "OCEANIA" }] }).count();
