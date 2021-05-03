2bc288721f4bf8ac9ff8d28596dd24894acbb64c
Ivanildo Cândido <systechtecnologia@gmail.com>
2021-05-03 15:11:54 -0300

    Corrigindo repetiçao desnecessaria

--- challenges/desafio28.js
+++ challenges/desafio28.js
@@ -1,13 +1,14 @@
+const company = 'LATAM AIRLINES BRASIL';
 db.resumoVoos.insertOne({
-  empresa: 'LATAM AIRLINES BRASIL',
+  empresa: company,
   totalVoosDomesticos: db.voos.count({
-    'empresa.nome': 'LATAM AIRLINES BRASIL',
+    'empresa.nome': company,
     natureza: 'Doméstica',
   }),
 });
 db.resumoVoos.findOne(
   {
-    empresa: 'LATAM AIRLINES BRASIL',
+    empresa: company,
   },
   {
     _id: 0,
