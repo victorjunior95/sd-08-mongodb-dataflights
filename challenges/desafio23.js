d69a1b144a081b0c9b9bace0ef0b0359f3840c69
Ian Zwanck Goodwin <ianzwanck@yahoo.com.br>
2021-05-03 18:35:53 -0300

    req23 & 24 fix

--- challenges/desafio23.js
+++ challenges/desafio23.js
@@ -1,6 +1,6 @@
 db.voos.find(
   {
-    $and: [{ litrosCombustivel: { $exists: true } }, { litrosCombustivel: { $gt: 1000 } }],
+    $and: [{ litrosCombustivel: { $exists: true } }, { litrosCombustivel: { $lt: 1001 } }],
   },
   { _id: 0, vooId: 1, litrosCombustivel: 1 },
 ).limit(1);
--- challenges/desafio24.js
+++ challenges/desafio24.js
@@ -10,5 +10,5 @@ db.voos.find(
       },
     ],
   },
-  { _id: 0, vooId: 1, "empresa.nome": 1, litrosCombustivel: 1 }
+  { _id: 0, vooId: 1, "empresa.nome": 1, litrosCombustivel: 1 },
 ).limit(1);
