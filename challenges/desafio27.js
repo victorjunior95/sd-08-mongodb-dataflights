9b46f083b49a2e51e1e555b4c5b73d72ee506f62
Ian Zwanck Goodwin <ianzwanck@yahoo.com.br>
2021-05-03 19:31:05 -0300

    req27 const

--- challenges/desafio27.js
+++ challenges/desafio27.js
@@ -1,5 +1,14 @@
+const empresaNome = "PASSAREDO";
+
 db.resumoVoos.insertOne({
-  empresa: "PASSARERO",
-  totalVoosDomesticos: db.voos.find({ "empresa.nome": "PASSARERO", natureza: "Doméstica" }).count(),
+  empresa: empresaNome,
+  totalVoosDomesticos: db.voos.find(
+    { "empresa.nome": empresaNome,
+    natureza: "Doméstica" },
+  ).count(),
 });
-db.resumoVoos.findOne({ empresa: "PASSARERO" }, { _id: 0, empresa: 1, totalVoosDomesticos: 1 });
+
+db.resumoVoos.findOne(
+  { empresa: empresaNome },
+  { _id: 0, empresa: 1, totalVoosDomesticos: 1 },
+  );
