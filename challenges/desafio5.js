5968a36a5114ef230caff923d067a01d8c109df5
JRGCast <joao.ralph1@gmail.com>
2021-05-03 18:58:07 -0300

    colocado id:0 no desafio 5

--- challenges/desafio5.js
+++ challenges/desafio5.js
@@ -1,2 +1,2 @@
 // 5 - Retorne o vooId do décimo ao décimo segundo documento da coleção voos.
-db.voos.find({}, { vooId: 1 }).skip(9).limit(3);
+db.voos.find({}, { vooId: 1, _id: 0 }).skip(9).limit(3);
