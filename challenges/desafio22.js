1e880be0d96fa3049030037724c2387ee10f060e
Fernanda PC <nandateixmarques@gmail.com>
2021-05-03 19:37:45 -0300

    corrigindo requisitos 22 e 6

--- challenges/desafio22.js
+++ challenges/desafio22.js
@@ -1,8 +1,8 @@
 db.voos.find({
   $and: [
-    { "empresa.nome": { $in: ["DELTA AIRLINES", "AMERICAN AIRLINES"] }},
+    { "empresa.nome": { $in: [ "DELTA AIRLINES", "AMERICAN AIRLINES" ] } },
     { "aeroportoOrigem.sigla": "SBGR" },
-    { "aeroportoDestino.sigla": "KJFK" }
-  ]},
-  { vooId: 1, _id: 0 }
+    { "aeroportoDestino.sigla": "KJFK" },
+  ] }, 
+  { vooId: 1, _id: 0 },
 ).limit(1);
--- challenges/desafio6.js
+++ challenges/desafio6.js
@@ -3,4 +3,3 @@ db.voos
     { vooId: 756807 },
     { _id: 0, "empresa.sigla": 1, "empresa.nome": 1, passageiros: 1 },
   );
-  
