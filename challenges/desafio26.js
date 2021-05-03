d7322fdd657623d2de6f7f5ba5ade65eb9f834bb
albertassi88 <albertassi88@gmail.com>
2021-05-03 19:02:59 -0300

    Alteracoes no desafio 26

--- challenges/desafio26.js
+++ challenges/desafio26.js
@@ -1,1 +1,1 @@
-db.voos.deleteMany({ "empresa.nome": "GOL", "passageiros.pagos": { $in: [5, 10] } });
+db.voos.deleteMany({ $and: [{ "empresa.nome": "GOL", "passageiros.pagos": { $in: [5, 10] } }] });
