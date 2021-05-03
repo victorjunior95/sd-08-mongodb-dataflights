7b2a3bada1185ac2cd19e2e0c20348ce19f39a46
Thiago Ferreira <thiagocristhian@gmail.com>
2021-05-03 15:31:57 -0300

    Requisito 22

--- challenges/desafio22.js
+++ challenges/desafio22.js
@@ -1,1 +1,10 @@
-db.voos.findOne({ $and: [{ $or: [{ "empresa.nome": "DELTA AIRLINES" }, { "empresa.nome": "AMERICAN AIRLIN S" }] }, { "aeroportoOrigem.sigla": "SBGR" }, { "aeroportoDestino.sigla": "KJFK" }] }, { _id: 0, vooId: 1 });
+db.voos.findOne({
+  $and: [
+    { $or: [
+      { 'empresa.nome': 'DELTA AIRLINES' },
+      { 'empresa.nome': 'AMERICAN AIRLIN S' },
+    ] },
+    { 'aeroportoOrigem.sigla': 'SBGR' },
+    { 'aeroportoDestino.sigla': 'KJFK' },
+  ],
+}, { _id: 0, vooId: 1 });
