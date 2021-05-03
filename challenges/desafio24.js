ccbadcf45dc20fd7dbe436d523d51580b0357c13
Alvaro Braga <alvaro.braga.job@gmail.com>
2021-05-03 18:14:14 -0300

    Req 22-24

---
+++ challenges/desafio24.js
@@ -0,0 +1,13 @@
+db.voos
+  .find(
+    {
+      $and: [
+        { litrosCombustivel: { $exists: true } },
+        { litrosCombustivel: { $lte: 600 } },
+        { "empresa.nome": { $ne: { $in: ["GOL", "AZUL"] } } },
+      ],
+    },
+    { _id: 0, vooId: 1, "empresa.nome": 1, litrosCombustivel: 1 },
+  )
+  .limit(1)
+  .pretty();
