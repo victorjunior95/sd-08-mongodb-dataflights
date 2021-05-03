13747c449e14f836a843ab6ba20529ba427d302a
Edson Araujo de Barros Junior <edson@pop-os.localdomain>
2021-05-03 17:11:30 -0300

    FixLint - Aspas duplas

--- challenges/desafio24.js
+++ challenges/desafio24.js
@@ -1,5 +1,5 @@
 db.voos.find({ $and: [{ litrosCombustivel: { $lte: 600 } },
 { litrosCombustivel: { $exists: true } },
-{ $nor: [{ 'empresa.nome': 'GOL' },
-{ 'empresa.nome': 'AZUL' }] }] },
-{ vooId: 1, litrosCombustivel: 1, 'empresa.nome': 1, _id: 0 }).limit(1);
+{ $nor: [{ "empresa.nome": "GOL" },
+{ "empresa.nome": "AZUL" }] }] },
+{ vooId: 1, litrosCombustivel: 1, "empresa.nome": 1, _id: 0 }).limit(1);
