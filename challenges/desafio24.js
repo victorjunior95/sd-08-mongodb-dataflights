cbec42ba122f5ac53bd35d4a434a051283e9069c
victor Farias <victorfarias.tv@gmail.com>
2021-05-03 16:23:13 -0300

    Adicionados desafios 21 ao 25

---
+++ challenges/desafio24.js
@@ -0,0 +1,8 @@
+db.voos.findOne({
+  $and: [
+    { litrosCombustivel: { $exists: true } },
+    { litrosCombustivel: { $not: { $gt: 600 } } },
+    { $nor: [
+      { 'empresa.nome': 'GOL' },
+      { 'empresa.nome': 'AZUL' }] }] }, {
+      _id: 0, vooId: 1, 'empresa.nome': 1, litrosCombustivel: 1 });
