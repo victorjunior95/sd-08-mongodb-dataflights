261f193440f97afbbe31a1ed733f5d8862581fc4
Wesley Paulo <wesley_paulo00@hotmail.com>
2021-05-03 19:32:35 -0300

    R23 - Concluido

---
+++ challenges/desafio23.js
@@ -0,0 +1,9 @@
+db.voos.findOne(
+  {
+    $and: [
+      { litrosCombustivel: { $lt: 1000 } },
+      { litrosCombustivel: { $exists: true } },
+    ],
+  },
+  { vooId: 1, litrosCombustivel: 1, _id: 0 },
+);
