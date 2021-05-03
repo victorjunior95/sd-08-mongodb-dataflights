365493065bcdcd3e7e1ce4089558a4069bf37725
Edson Araujo de Barros Junior <edson@pop-os.localdomain>
2021-05-03 18:32:58 -0300

    desafio28

---
+++ challenges/desafio28.js
@@ -0,0 +1,6 @@
+db.resumoVoos.insert({ empresa: "LATAM AIRLINES BRASIL",
+totalVoosDomesticos: db.voos.count({ $and: [{ "empresa.nome": "LATAM AIRLINES BRASIL" },
+{ natureza: "Doméstica" }] }) });
+
+db.resumoVoos.find({ empresa: "LATAM AIRLINES BRASIL" },
+{ empresa: 1, totalVoosDomesticos: 1, _id: 0 }).limit(1);
