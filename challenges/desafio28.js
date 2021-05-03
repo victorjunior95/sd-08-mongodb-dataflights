82edaae65f263ae6cf79d5eec9528d2144c969fa
Doug-Fernando <doug.fernando95@gmail.com>
2021-05-03 17:21:17 -0300

    [add] - Req 9 ao 28

---
+++ challenges/desafio28.js
@@ -0,0 +1,10 @@
+const EMPRESA = 'LATAM AIRLINES BRASIL';
+db.resumoVoos.insertOne({
+  empresa: EMPRESA,
+  totalVoosDomesticos: db.voos.find({
+    'empresa.nome': EMPRESA,
+    natureza: 'Doméstica',
+  }).count(),
+});
+
+db.resumoVoos.find({ empresa: EMPRESA }, { _id: 0 });
