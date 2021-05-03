8963456d775c5dee823e8b89cc64c6e33858645e
Bruno Martins <bruno.martins217@gmail.com>
2021-05-03 20:33:56 -0300

    Desafio 23

--- challenges/desafio23.js
+++ challenges/desafio23.js
@@ -1,3 +1,5 @@
-db.voos.find({ 
-  $and: [{ litrosCombustivel: { $exists: true } }, { litrosCombustivel: { $gt: 1000 } }] }, 
-  { vooId: 1, litrosCombustivel: 1, _id: 0 }).limit(1);
+db.voos.find(
+  { $and: [{ litrosCombustivel: { $exists: true } },
+  { litrosCombustivel: { $not: { $gt: 1000 } } }] },  
+  { vooId: 1, litrosCombustivel: 1, _id: 0 },
+).limit(1);
