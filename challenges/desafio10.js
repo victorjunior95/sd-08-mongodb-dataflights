e5751b26791e632eafdc9c39b0cf8921ca51a4e2
Wesley Paulo <wesley_paulo00@hotmail.com>
2021-05-03 18:30:14 -0300

    R10, R12 - Concluido

--- challenges/desafio10.js
+++ challenges/desafio10.js
@@ -1,8 +1,10 @@
 db.voos.find(
     {
-        "empresa.nome": "GOL", ano: 2017,
+        "empresa.nome": "GOL",
+        ano: 2017,
     },
     {
+        _id: 0,
         vooId: 1, 
         "empresa.nome": 1,
         "aeroportoOrigem.nome": 1,
