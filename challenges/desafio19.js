4c6539876baecb5fd54697fc60512232a11b2056
JRGCast <joao.ralph1@gmail.com>
2021-05-03 17:08:17 -0300

    resolvido desafio 19

---
+++ challenges/desafio19.js
@@ -0,0 +1,3 @@
+// 19 - Retorne o vooId do primeiro voo em que o campo litrosCombustivel exista.
+db.voos.findOne({ litrosCombustivel: { $exists: 1 } }, { vooId: 1, mes: 1, ano: 1, _id: 0 });
+// true também funciona no lugar do 1 em $exists
