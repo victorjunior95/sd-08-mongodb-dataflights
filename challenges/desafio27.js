bd127d180e244f23e64c72f960487e8d3e46a728
Fernanda PC <nandateixmarques@gmail.com>
2021-05-03 22:49:39 -0300

    adicionando mudanças finais

--- challenges/desafio27.js
+++ challenges/desafio27.js
@@ -1,11 +1,12 @@
-db.resumoVoos.insertOne({
-  empresa: "PASSAREDO",
-  totalVoosDomesticos: db.voos
-    .find({ "empresa.nome": "PASSAREDO", natureza: "Doméstica" })
-    .count(),
-});
+const totalVoosDomesticos = db.voos.find(
+  { "empresa.nome": "PASSAREDO", natureza: "Doméstica" },
+  ).count();
+
+const empresa = "PASSAREDO";
+
+db.resumoVoos.insertOne({ empresa: "PASSAREDO", totalVoosDomesticos });
 
 db.resumoVoos.find(
   { empresa: "PASSAREDO" },
-  { empresa: 1, totalVoosDomesticos: 1, _id: 0 },
-);
+  { _id: 0, empresa: 1, totalVoosDomesticos: 1 },
+).limit(1);
