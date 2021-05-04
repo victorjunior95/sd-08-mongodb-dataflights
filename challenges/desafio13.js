ca1c196b954404afb69c133f057e6aca94a6d3d5
Leandro Lachini <leandrolachini@gmail.com>
2021-05-04 19:00:31 -0300

    desafio 13

--- challenges/desafio13.js
+++ challenges/desafio13.js
@@ -1,4 +1,1 @@
-db.voos.find({
-  $or: [{ $not: { "aeroportoDestino.continente": "EUROPA" } },
-  { $not: { "aeroportoDestino.continente": "ÁSIA" } },
-  { $not: { "aeroportoDestino.continente": "OCEANIA" } }] }).count();
+db.voos.find({ "aeroportoDestino.continente": { $nin: ["EUROPA", "ÁSIA", "OCEANIA"] } }).count();
