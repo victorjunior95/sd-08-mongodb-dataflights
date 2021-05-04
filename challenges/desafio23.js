6936adaca2f65508b08d5e314999cc5cd703ba42
Thais Quintela <thais.s.quintela@gmail.com>
2021-05-04 15:05:39 -0300

    Requisitos 11 ao 26

---
+++ challenges/desafio23.js
@@ -0,0 +1,7 @@
+// 23 - Retorne o vooId e litrosCombustivel do primeiro voo em que o campo litrosCombustivel não seja maior do que 1000 e o campo litrosCombustivel exista.
+db.voos.findOne(
+  {
+    litrosCombustivel: { $lte: 1000 },
+  },
+    { vooId: 1, litrosCombustivel: 1, _id: 0 },
+);
---
+++ challenges/desafio27.js
@@ -0,0 +1,2 @@
+// 27 - Retorne a quantidade total de voos de natureza Doméstica que a empresa PASSAREDO possui, via uso de uma nova coleção chamada resumoVoos.
+
