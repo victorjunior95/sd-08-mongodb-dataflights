96e7fa5817812f0441d07a8345cfd6d3f7575ce2
Thalys Matias Carrara <thalysmatiascarrara@gmail.com>
2021-05-03 21:57:13 -0400

    requisitos 20, 21,22, 23, 24, 25, 26 e 27

---
+++ challenges/desafio27.js
@@ -0,0 +1,17 @@
+const company = "PASSAREDO";
+
+const totalVoos = db.voos.find(
+  { "empresa.nome": company,
+    natureza: "Doméstica",  
+  },
+).count();
+
+db.resumoVoos.insertOne({
+  empresa: company,
+  totalVoosDomesticos: totalVoos,
+});
+
+db.resumoVoos.find(
+  { empresa: company },
+  { _id: 0 },
+);
