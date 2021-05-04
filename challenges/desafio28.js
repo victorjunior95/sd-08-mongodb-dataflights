c80f2287277116a6a2ede39d2d656ff49340721f
Fernanda PC <nandateixmarques@gmail.com>
2021-05-03 22:41:27 -0300

    adicionando desafio 28 - corrigindo erros

--- challenges/desafio28.js
+++ challenges/desafio28.js
@@ -1,10 +1,11 @@
 const totalVoosDomesticos = db.voos.find(
-  {"empresa.nome": "LATAM AIRLINES BRASIL", "natureza": "Doméstica" }).count;
+  { "empresa.nome": "LATAM AIRLINES BRASIL", natureza: "Doméstica" }
+  ).count();
 
 const empresa = "LATAM AIRLINES BRASIL";
 
 db.resumoVoos.insertOne({ empresa, totalVoosDomesticos });
 
 db.resumoVoos.find(
-  { empresa }, { _id: 0, empresa: 1, totalVoosDomesticos: 1 }
-);
+  { empresa }, { _id: 0, empresa: 1, totalVoosDomesticos: 1 },
+).limit(1);
