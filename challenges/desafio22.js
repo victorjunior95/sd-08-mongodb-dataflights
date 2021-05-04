4c46b015932e713670cd2d8fd49471a638b27996
Breenolf <blf@ic.ufal.br>
2021-05-04 02:03:35 -0300

    Desafios 21 e 22

---
+++ challenges/desafio22.js
@@ -0,0 +1,12 @@
+db.voos.find({
+    $and: [
+      {
+        "aeroportoOrigem.sigla": "SBGR",
+      },
+      { "aeroportoDestino.sigla": "KJFK",
+      },
+      {
+        $or: [{ "empresa.nome": "DELTA AIRLINES" }, { "empresa.nome": "AMERICAN AIRLINES" }],  
+      },
+    ],
+}, { vooId: 1, _id: 0 }).limit(1);
