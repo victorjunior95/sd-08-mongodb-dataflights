eb1e26f821e239d26e5c5640065b6ebe483cf17f
Leandro Lachini <leandrolachini@gmail.com>
2021-05-05 19:12:00 -0300

    desafio 27

---
+++ challenges/desafio27.js
@@ -0,0 +1,7 @@
+db.resumoVoos.insert({
+  empresa: "PASSAREDO" },
+  { totalVoosDomesticos: db.voos.find({ $and:
+    [{ "empresa.nome": "PASSAREDO" }, { natureza: "Doméstica" }] }).count() });
+
+db.resumoVoos.findOne({ empresa: "PASSAREDO" },
+{ _id: false, empresa: true, totalVoosDomesticos: true });
