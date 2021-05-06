552d4ce0b6c7e9bcbd45dca37ef48daf7d41b030
Eduardo Bonizio <eduardo.bonizio@gmail.com>
2021-05-06 20:28:41 -0300

    Requisito 12

--- challenges/desafio13.js
+++ challenges/desafio13.js
@@ -1,1 +1,2 @@
 // 13 - Retorne a quantidade de documentos em que o campo aeroportoDestino.continente não seja igual a EUROPA, ÁSIA e OCEANIA.
+db.voos.count({ "aeroportoDestino.pais": { $nin: ["EUROPA", "ÁSIA", "OCEANIA"] } });
