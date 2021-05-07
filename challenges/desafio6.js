f5b3dd51e573f4cd9954d015ba12968e5344484b
Ruâni Filipe <dev.ruanifilipe@gmail.com>
2021-05-07 10:29:15 -0300

    correcao req6

--- challenges/desafio6.js
+++ challenges/desafio6.js
@@ -1,4 +1,4 @@
 db.voos.find(
-  { _id: 0, vooId: 756807 },
-  { "empresa.sigla": 1, "empresa.nome": 1, passageiros: 1 },
+  { vooId: 756807 },
+  { _id: 0, "empresa.sigla": 1, "empresa.nome": 1, passageiros: 1 },
   );
