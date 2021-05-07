7bb3314a11873ebda345a7763afa017af4470008
Leonardo <lecastro16@gmail.com>
2021-05-07 12:43:33 -0300

    requisito 28

--- challenges/desafio28.js
+++ challenges/desafio28.js
@@ -1,1 +1,8 @@
-// Leonardo
+db.resumoVoos.insertOne({
+  empresa: "LATAM AIRLINES BRASIL",
+  totalVoosDomesticos: db.voos
+    .find({ "empresa.nome": "LATAM AIRLINES BRASIL", natureza: "Doméstica" })
+    .count(),
+});
+
+db.resumoVoos.find({}, { empresa: 1, totalVoosDomesticos: 1, _id: 0 });
