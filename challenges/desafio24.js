af473c5f1534b97b34cc7e674dc61dcf612ec097
wagnerberna <cabelo29007@gmail.com>
2021-05-06 21:01:03 -0300

    até req24

--- challenges/desafio24.js
+++ challenges/desafio24.js
@@ -4,4 +4,4 @@ db.voos.findOne({
     { "empresa.nome": { $nin: ["GOL", "AZUL"] } },
   ],
 },
-{ _id: 0, "empresa.nome": 1, litrosCombustivel: 1 });
+{ _id: 0, vooId: 1, "empresa.nome": 1, litrosCombustivel: 1 });
