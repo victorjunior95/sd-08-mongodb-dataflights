9cd32fcf6c2ca3df4579b3d8723acaed633c8c5a
Eduardo Bonizio <eduardo.bonizio@gmail.com>
2021-05-06 21:36:29 -0300

    Requisito 24

--- challenges/desafio24.js
+++ challenges/desafio24.js
@@ -1,1 +1,6 @@
 // 24 - Retorne o vooId, empresa.nome e litrosCombustivel do primeiro voo em que litrosCombustivel não seja maior do que 600 e a empresa não seja GOL ou AZUL, e o campo litrosCombustivel exista.
+db.voos.findOne({ $and: [
+  { litrosCombustivel: { $exists: true } },
+  { litrosCombustivel: { $lte: 600 } },
+  { "empresa.nome": { $not: { $in: ["GOL", "AZUL"] } } },
+] }, { vooId: 1, "empresa.nome": 1, litrosCombustivel: 1, _id: 0 });
