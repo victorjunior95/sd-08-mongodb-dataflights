05e1554880d133cf6af2996aed81d8c292699b36
Uilton Rocha <itoclick@gmail.com>
2021-05-07 11:56:59 -0300

    desafio 1-28

--- challenges/desafio28.js
+++ challenges/desafio28.js
@@ -1,8 +1,8 @@
 const tam = "LATAM AIRLINES BRASIL";
-db.voos.updateMany({ "empresa.nome": `${tam}`, natureza: "Doméstica" },
- { $set: { empresa: `${tam}` }, totalVoosDomesticos: 20026 });
+db.voos.updateMany({ "empresa.nome": tam, natureza: "Doméstica" },
+ { $set: { empresa: tam, totalVoosDomesticos: 20026 } });
 
 db.voos.find({ 
   natureza: "Doméstica", 
-  empresa: "LATAM AIRLINES BRASIL" },
+  empresa: tam },
   { empresa: 1, totalVoosDomesticos: 1, _id: 0 }).limit(1).pretty();
