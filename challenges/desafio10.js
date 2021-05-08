1d50ddbc6fe79923554679c3d42204573a409d33
Giovanna Betti <giovannabetti@gmail.com>
2021-05-08 02:23:30 -0300

    fixing eslint

--- challenges/desafio10.js
+++ challenges/desafio10.js
@@ -7,5 +7,5 @@ db.voos.find(
 "aeroportoDestino.nome": 1,
 mes: 1,
 ano: 1,
-_id: 0, },
+_id: 0 },
 ).limit(10);
--- challenges/desafio6.js
+++ challenges/desafio6.js
@@ -1,1 +1,4 @@
-db.voos.findOne({ vooId: 756807 }, { "empresa.sigla": 1, "empresa.nome": 1, passageiros: 1, _id: 0 });
+db.voos.findOne(
+{ vooId: 756807 },
+{ "empresa.sigla": 1, "empresa.nome": 1, passageiros: 1, _id: 0 },
+);
