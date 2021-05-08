adcbe616854054cc844761910fbf32a8f09b06a6
Natanael Neto <natanael.eneas2018@hotmail.com>
2021-05-08 13:34:19 -0300

    C21 - 23rd Req Sent

---
+++ challenges/desafio23.js
@@ -0,0 +1,14 @@
+db.voos.find(
+  {
+    litrosCombustivel:
+    {
+      $lt: 1000,
+      $exists: true,
+    },
+  },
+  {
+    _id: 0,
+    vooId: 1,
+    litrosCombustivel: 1,
+  },
+).limit(1);
