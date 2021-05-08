6dd6ce8cb0d28d75f3869100610d403381e50da2
Natanael Neto <natanael.eneas2018@hotmail.com>
2021-05-08 13:54:24 -0300

    C25 - 27th Req Sent

---
+++ challenges/desafio27.js
@@ -0,0 +1,23 @@
+db.resumoVoos.insertOne(
+  {
+    empresa: "PASSAREDO",
+    totalVoosDomesticos:
+    db.voos.find(
+      {
+        "empresa.nome": "PASSAREDO",
+        natureza: "Doméstica",
+      },
+    ).count(),
+  },
+);
+
+db.resumoVoos.find(
+  {
+    empresa: "PASSAREDO",
+  },
+  {
+    _id: 0,
+    empresa: 1,
+    totalVoosDomesticos: 1,
+  },
+);
