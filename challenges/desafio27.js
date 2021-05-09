026917a622f38bb5d045497907625cd2d9075a5e
Silvio Dayube <scarige@gmail.com>
2021-05-08 21:50:58 -0300

    27th challenge done

---
+++ challenges/desafio27.js
@@ -0,0 +1,11 @@
+// db.voos.count({
+//   'empresa.nome': 'PASSAREDO',
+//   natureza: 'Doméstica'
+// });
+
+db.resumoVoos.insertOne({
+  empresa: 'PASSAREDO',
+  totalVoosDomesticos: 4187,
+});
+
+db.resumoVoos.findOne({ empresa: 'PASSAREDO' }, { _id: 0 });
