4ed6a369f5e278ff1aaedb3c426709dc83740170
Silvio Dayube <scarige@gmail.com>
2021-05-08 21:51:04 -0300

    28th challenge done

---
+++ challenges/desafio28.js
@@ -0,0 +1,11 @@
+// db.voos.count({
+//   'empresa.nome': 'LATAM AIRLINES BRASIL',
+//   natureza: 'Doméstica'
+// });
+
+db.resumoVoos.insertOne({
+  empresa: 'LATAM AIRLINES BRASIL',
+  totalVoosDomesticos: 20026,
+});
+
+db.resumoVoos.findOne({ empresa: 'LATAM AIRLINES BRASIL' }, { _id: 0 });
