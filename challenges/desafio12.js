0f3593ae578666d935c4c4ef5a231ce3afe1ab21
MarceloBorges <melombm@hotmail.com>
2021-05-09 11:13:29 -0500

    requisito 12 - operador de logica or que e igual a ou um  documentos que satisfaçam ao menos uma das expressões

---
+++ challenges/desafio12.js
@@ -0,0 +1,9 @@
+db.voos.count(
+  {
+    $or: [
+      { "aeroportoDestino.pais": { $eq: "BRASIL" } },
+      { "aeroportoDestino.pais": { $eq: "ARGENTINA" } },
+      { "aeroportoDestino.pais": { $eq: "CHILE" } },
+    ],
+  },
+);
