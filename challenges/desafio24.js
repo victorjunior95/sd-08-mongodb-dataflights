2ac84d1309ddbb90b2eb42f7ccab3ab88d941a9e
Bianca <bruninhakjurugat11@gmail.com>
2021-05-09 17:02:11 -0300

    desafio24 done

--- challenges/desafio24.js
+++ challenges/desafio24.js
@@ -1,1 +1,3 @@
-db.voos.findOne({'litrosCombustivel': { $exists: 1, $lte: 600 }, 'empresa.nome': { $nin: ['GOL', 'AZUL'] }} , {'_id': 0, 'vooId': 1, 'empresa.nome': 1, 'litrosCombustivel': 1});
+db.voos.findOne({ litrosCombustivel: { $exists: 1, $lte: 600 }, 
+    "empresa.nome": { $nin: ["GOL", "AZUL"] } }, 
+    { _id: 0, vooId: 1, "empresa.nome": 1, litrosCombustivel: 1 });
