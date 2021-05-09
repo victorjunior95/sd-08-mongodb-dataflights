6f6520ccee100187d15f4459a64501f386e65ce2
Bianca <bruninhakjurugat11@gmail.com>
2021-05-09 17:02:11 -0300

    desafio28 done

--- challenges/desafio28.js
+++ challenges/desafio28.js
@@ -1,5 +1,5 @@
-let counter = db.voos.count({ 'empresa.nome': 'LATAM AIRLINES BRASIL', 'natureza': 'Doméstica' });
+const counter = db.voos.count({ "empresa.nome": "LATAM AIRLINES BRASIL", natureza: "Doméstica" });
 
-db.resumoVoos.insertOne({ 'empresa': 'LATAM AIRLINES BRASIL', 'totalVoosDomesticos': counter });
+db.resumoVoos.insertOne({ empresa: "LATAM AIRLINES BRASIL", totalVoosDomesticos: counter });
 
-db.resumoVoos.findOne({}, { '_id': 0 });
+db.resumoVoos.findOne({}, { _id: 0 });
