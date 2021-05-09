7b3349724386acb6a34c6b93e804d2a23576d1b0
jnpefo <jnpefo@gmail.com>
2021-05-09 20:09:14 -0300

    Req 18

--- scripts/generate_result.sh
+++ scripts/generate_result.sh
@@ -30,7 +30,7 @@ print_results() {
 # Print tests evaluation
 # for entry in "$TRYBE_DIR/expected-results"/*
 # do
-for i in $(seq 17 17)
+for i in $(seq 18 18)
 do
   entry="$TRYBE_DIR/expected-results/desafio$i"
   scripts/resetdb.sh "$DB_RESTORE_DIR"
