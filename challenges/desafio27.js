651855415728914f39709e1144fd94dfab691b6a
MarceloBorges <melombm@hotmail.com>
2021-05-10 03:51:09 -0500

    adicionando comentario

--- challenges/desafio27.js
+++ challenges/desafio27.js
@@ -20,3 +20,6 @@ db.resumoVoos.find(
   { empresa: "PASSAREDO" },
   { _id: 0, empresa: 1, totalVoosDomesticos: 1 },
 );
+
+// https://github.com/tryber/sd-08-mongodb-dataflights/blob/RitaJeveaux-mongodb-dataflights/challenges/desafio28.js
+// pegando a ordem da query com aluna ritaJeveaux
