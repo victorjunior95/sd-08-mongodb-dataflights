a222cbd600d73985128376b1bae4b1d503eb9e09
MarceloBorges <melombm@hotmail.com>
2021-05-09 23:20:52 -0500

    requisito 24 - findOne returna um unico documento de uma colecao , lt e menor que , nin seleciona os documentos em que o valor do campo filtrado não é igual ao especificado no array , ou o campo não existe.

---
+++ challenges/desafio24.js
@@ -0,0 +1,14 @@
+db.voos.findOne({ 
+    $and: 
+    [
+      { litrosCombustivel: { $lt: 600 } },
+      { "empresa.nome": { $nin: ["GOL", "AZUL"] } },
+      { litrosCombustivel: { $exists: true } },
+    ],
+  },
+  { 
+    _id: 0, 
+    vooId: 1,
+    "empresa.nome": 1, 
+    litrosCombustivel: 1,
+  });
