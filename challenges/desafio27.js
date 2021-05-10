de7bb1cbac476ad042ceb4b043fc5935653f3924
devFelipeFranca <felipefrancafj@gmail.com>
2021-05-10 14:53:30 -0300

    Update...

---
+++ challenges/desafio27.js
@@ -0,0 +1,10 @@
+const nomeEmpresa = "PASSAREDO";
+const QntVoosDomesticos = db.voos.find({ $and: [
+  { "empresa.nome": nomeEmpresa },
+  { natureza: "Doméstica" },
+] }).count();
+
+db.resumoVoos.insertOne({ empresa: nomeEmpresa, totalVoosDomesticos: QntVoosDomesticos });
+
+db.resumoVoos.find({ empresa: nomeEmpresa }, { _id: 0, empresa: 1, totalVoosDomesticos: 1 })
+.limit(1);
