0884d1d0007215e77800b38ef3a818386da3f542
adrianWarrior <adrianoforcellinipedretti@gmail.com>
2021-05-16 18:09:00 -0300

    nova tentativa de correçao do 24

--- challenges/desafio24.js
+++ challenges/desafio24.js
@@ -1,9 +1,11 @@
-db.voos.find({
-    $and: [
-        { litrosCombustivel: { $lt: 601 } },
-        { "empresa.nome": { $nin: ["GOL", "AZUL"] } },
-    ],
-},
-    { _id: 0, vooId: 1, "empresa.nome": 1, litrosCombustivel: 1 },
-)
-.limit(1);
+db.voos
+    .find(
+        {
+            $and: [
+                { litrosCombustivel: { $lt: 601 } },
+                { "empresa.nome": { $nin: ["GOL", "AZUL"] } },
+            ],
+        },
+        { _id: 0, vooId: 1, "empresa.nome": 1, litrosCombustivel: 1 },
+    )
+    .limit(1);
