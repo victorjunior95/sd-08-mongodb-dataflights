6ccd94ffe0aba4854ac19c45116e429349785946
adrianWarrior <adrianoforcellinipedretti@gmail.com>
2021-05-16 15:49:52 -0300

    desafio 13 formataçao

--- challenges/desafio13.js
+++ challenges/desafio13.js
@@ -1,2 +1,2 @@
-db.voos.find({ "aeroportoDestino.continente": { $nin: ["EUROPA", "ÁSIA", "OCEANIA"] }}).count();
+db.voos.find({ "aeroportoDestino.continente": { $nin: ["EUROPA", "ÁSIA", "OCEANIA"] } }).count();
 // https://docs.mongodb.com/manual/reference/operator/query/nin/
