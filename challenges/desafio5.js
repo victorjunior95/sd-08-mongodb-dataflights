4ffbc8f2cd3cca1d3a0c263df8a9fb1b7246d997
adrianWarrior <adrianoforcellinipedretti@gmail.com>
2021-05-16 20:49:06 -0300

    correçao de comentario

--- challenges/desafio5.js
+++ challenges/desafio5.js
@@ -1,5 +1,4 @@
 db.voos.find({}, { vooId: 1, _id: 0 }).skip(9).limit(3);
-// se colocasse apenas vooId: 1 ou vooId: true,
-// a busca retornaria junto tudo que vem antes de vooId (_id nesse caso)
+// é sempre necessario exluir o id gerado pelo mongo da busca, caso não o queira ver
 // consultei o repositório de Arnaelcio 
 // https://github.com/tryber/sd-08-mongodb-dataflights/pull/85/files
