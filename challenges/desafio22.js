52aed3f4102c9c77d35996c947d22f134c205b23
Alana Monteiro <lanaah.15@gmail.com>
2021-05-19 21:51:44 -0300

    commit inicial - Alana Monteiro

---
+++ challenges/desafio22.js
@@ -0,0 +1,5 @@
+db.voos.findOne({ $and: [
+{ "empresa.nome": { $in: ["DELTA AIRLINES", "AMERICAN AIRLINES"] } },
+{ "aeroportoOrigem.sigla": { $eq: "SBGR" } },
+{ "aeroportoDestino.sigla": { $eq: "KJFK" } }] },
+{ _id: 0, vooId: 1 });
