4b14296ff1d68c6680dc434f542278fe8a950b33
Marcela <marcela9908@gmail.com>
2021-05-29 14:27:07 -0300

    Desafio 12

---
+++ challenges/desafio12.js
@@ -0,0 +1,7 @@
+db.voos.count(
+  { $or: [
+    { "aeroportoDestino.pais": { $eq: "BRASIL" } },
+    { "aeroportoDestino.pais": { $eq: "ARGENTINA" } },
+    { "aeroportoDestino.pais": { $eq: "CHILE" } },
+  ] },   
+);
