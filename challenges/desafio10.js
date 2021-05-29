c983614ef8f23735f3ce1b57cf1853569ed9ff08
Marcela <marcela9908@gmail.com>
2021-05-29 13:18:34 -0300

    Desafio 11

--- challenges/desafio10.js
+++ challenges/desafio10.js
@@ -1,5 +1,3 @@
-/* 10 - Retorne apenas os 10 primeiros documentos com voos da empresa GOL do ano de 2017. 
-Exiba apenas os campos vooId, empresa.nome, aeroportoOrigem.nome, aeroportoDestino.nome, mes e ano. */
 db.voos.find(
   { $and: [{ "empresa.nome": "GOL" }, { ano: 2017 }] }, 
   { vooId: 1, 
