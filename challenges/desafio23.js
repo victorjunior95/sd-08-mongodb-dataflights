1a073714eb4e405d1c65ac251064acecc81e16d7
Elisa <elisatcamillo@gmail.com>
2021-05-30 14:09:24 -0300

    23 - exists

--- challenges/desafio23.js
+++ challenges/desafio23.js
@@ -1,3 +1,4 @@
-db.voos.find({ 
-  litrosCombustivel: { $not: { $gt: 1000 } } }, 
-  { vooId: 1, _id: 0, litrosCombustivel: 1 }).limit(1);
+db.voos.find(
+  { litrosCombustivel: { $exists: true, $not: { $gt: 1000 } } },
+  { vooId: 1, _id: 0, litrosCombustivel: 1 },
+).limit(1);
